b1367d8667ba62c40147a9f9212c9c0acca86948
amir <mashhadifarahani1@gmail.com>
2023-08-19 12:04:50 +0330

    fix display only flex and rewrite navbar

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ document.querySelector(".footer-right button").addEventListener("click",handleCl
 function handleClick(){
     alert("YOUR MESSAGE SUBMITTED ");
 }
+
+let currentSection = 0;
+
+const sections = document.querySelectorAll('.section');
+
+window.addEventListener('wheel',f);
+
+function f(event){
+    event.preventDefault();
+    const delta = event.deltaY;
+    if(delta >0 && currentSection < sections.length-1){
+        currentSection++;
+    }
+    else if(delta<0 && currentSection>0){
+        currentSection--;
+    }
+    sections[currentSection].scrollIntoView({behavior:'smooth'});
+};
