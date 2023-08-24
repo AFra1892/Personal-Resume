3ed2212831af7bdbef093926924af379c85a499c
amir <mashhadifarahani1@gmail.com>
2023-08-24 19:46:00 +0330

    puting this project aside for now - this project is almost compelete

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function handleClick(){
 
 let currentSection = 0;
 
-const sections = document.querySelectorAll('.section');
+const sectionss = document.querySelectorAll('.section');
 
 window.addEventListener('wheel',f);
 
@@ -22,3 +22,21 @@ function f(event){
     sections[currentSection].scrollIntoView({behavior:'smooth'});
     
 };
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+window.onscroll = ()=>{
+    sections.forEach(sec=>{
+        let tip = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id  = sec.getAttribute('id')
+
+        if(top>=offset && top < offset +height){
+            navLinks.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelectorAll('header nav a[href*='+id+']').classList.add('active');
+            });
+        }
+    });
+}
